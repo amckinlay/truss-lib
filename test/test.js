c197fa74bd46e6b7886d0b25212e3b31c6858164
amckinlay <mckinlay.andrew@gmail.com>
2013-04-23 12:17:49 -0300

    Added comments

--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
 (function() {
+	// Forming truss below
 	var joint1 = Object.create(t.joint);
 	var joint2 = Object.create(t.joint);
 	var joint3 = Object.create(t.joint);
+	// Positions of joints in space
 	joint1.x = 7;
 	joint1.y = -3;
 	joint2.x = -2;
@@ -23,6 +25,7 @@
 	joint2.addMember(mem2);
 	joint3.addMember(mem2);
 	joint3.addMember(mem3);
+	// Adding a load and bracing forces
 	var force1 = Object.create(t.force);
 	force1.magnitude = 2;
 	force1.angle = -Math.PI / 2;
@@ -40,19 +43,33 @@
 	truss.addJoint(joint1);
 	truss.addJoint(joint2);
 	truss.addJoint(joint3);
+	
+	// Three unknown members
+	console.log(mem1.known());
+	console.log(mem2.known());
+	console.log(mem3.known());
+	
+	// Solve
 	truss.solve();
+	
+	// Tree unknown members now known
+	console.log(mem1.known());
+	console.log(mem2.known());
+	console.log(mem3.known());
+	
+	// We can not get their magnitudes and compression/tension bool
 	console.log(mem1.magnitude);
 	console.log(mem2.magnitude);
 	console.log(mem3.magnitude);
 	console.log(mem1.compression);
 	console.log(mem2.compression);
 	console.log(mem3.compression);
+	
+	// Also, the bracing forces are now known
 	console.log(brace1.x());
 	console.log(brace1.y());
 	console.log(brace2.x());
 	console.log(brace2.y());
 	console.log(brace3.x());
 	console.log(brace3.y());
-	//console.log(brace2.compression);
-	//console.log(brace3.compression);
 })();
